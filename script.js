80eddd2c0e8af822bf4351a3bffded7f05211698
totekits <144774991+totekits@users.noreply.github.com>
2025-06-08 07:42:15 +0700

    Move script from index.html to script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(x, y) {
+      return x + y;
+    }
+
+    function subtract(x, y) {
+      return x - y;
+    }
+
+    function multiply(x, y) {
+      return x * y;
+    }
+
+    function divide(x, y) {
+      return x / y;
+    }
+    
+    let operand1;
+    let operand2;
+    let operator;
+
+    function operate(x, operatorFunction, y) {
+      return operatorFunction(x, y);
+    }
